97cb43f939b7d4c613b7a4358ab37211671fc196
David Marom <davidmarom@hotmail.com>
2026-01-28 19:29:45 +0200

    added data service and card components

--- src/app/computer/page.js
+++ src/app/computer/page.js
@@ -1,12 +1,29 @@
 'use client';
 
 import React, { useState } from 'react';
+import { getComputers } from '@/services/data.js';
+import { Card } from '@/app/components/Card/Card.jsx';
+
 
 export default function Home() {
+
 
     return (
         <div className='page-container '>
             <h1>Computer Page</h1>
-        </div>  
+
+            <div className="row">
+
+                {
+                    getComputers().map(
+                        (computer) => {
+                            return <Card data={computer} />
+                        }
+                    )
+                }
+
+            </div>
+
+        </div>
     );
 }
